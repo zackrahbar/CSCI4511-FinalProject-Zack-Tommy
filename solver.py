e1b79388eee401c84e7431b21384caa4a688dff8
Zack rahbar <zackrahbar@gwu.edu>
2024-12-10 17:05:38 -0500

    init stuff

--- solver.py
+++ solver.py
@@ -4,9 +4,18 @@ from random import randint
 from game.src.objects import  Player
 
 class POMDPPlayer(Player):
-	def __init__(self):
-		#create a new player
-		#initialize variables for things like learnign rate and q table
+    def __init__(self, name, player_num):
+        # Create a new player
+        # Initialize variables for things like learning rate and Q-table
+
+        super().__init__(name, player_num)
+
+        # Initialize variables
+        self.learning_rate = 0.1
+        self.discount_rate = 0.99
+        
+        # Initialize Q-table
+        self.q_table = {}
             
     def get_options(self):
         #OVERWRITES PLAYERS METHOD
