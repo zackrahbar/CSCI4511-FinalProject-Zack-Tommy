2645b818b5459325ec09c7e68f0720564000cc30
Zack rahbar <zackrahbar@gwu.edu>
2024-12-10 16:44:36 -0500

    framework?

---
+++ solver.py
@@ -0,0 +1,20 @@
+from game.src.constants import *
+from uuid import uuid4
+from random import randint
+from game.src.objects import  Player
+
+class POMDPPlayer(Player):
+	def __init__(self):
+		#create a new player
+		#initialize variables for things like learnign rate and q table
+            
+    def get_options(self):
+        #get all possible actions 
+		
+    def make_a_move(self):
+        #choose an action via q learning 
+		
+    def make_bet(self):
+        #OVERWRITES PLAYERS METHOD
+        #calcualte a bet based on current bank
+		
