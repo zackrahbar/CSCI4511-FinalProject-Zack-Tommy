b873e06484f75a7fc0230c0f502e42759236535a
Zack rahbar <zackrahbar@gwu.edu>
2024-12-10 19:21:04 -0500

    updates

--- game/solver.py
+++ game/solver.py
@@ -3,31 +3,34 @@ from uuid import uuid4
 from random import randint
 from src.objects import  Player
 
-# class POMDPPlayer(Player):
-#     def __init__(self, name, player_num):
-#         # Create a new player
-#         # Initialize variables for things like learning rate and Q-table
+class POMDPPlayer(Player):
+    def __init__(self, name, player_num):
+        # Create a new player
+        # Initialize variables for things like learning rate and Q-table
 
-#         super().__init__(name, player_num)
+        super().__init__(name, player_num)
 
-#         # Initialize variables
-#         self.learning_rate = 0.1
-#         self.discount_rate = 0.99
+        # Initialize variables
+        self.learning_rate = 0.1
+        self.discount_rate = 0.99
         
-#         # Initialize Q-table
-#         self.q_table = {}
+        # Initialize Q-table
+        self.q_table = {}
+
+        #initialize seen cards 
+        self.seen = []
             
-#     def get_options(self):
-#         #OVERWRITES PLAYERS METHOD
-#         #get all possible actions 
-#         #CALLS MAKE A MOVE PASSING IN THE POSSIBLE ACTIONS AND RETURNS WHAT THAT RETURNS 
+    def get_options(self):
+        #OVERWRITES PLAYERS METHOD
+        #get all possible actions 
+        #CALLS MAKE A MOVE PASSING IN THE POSSIBLE ACTIONS AND RETURNS WHAT THAT RETURNS 
 		
-#     def make_a_move(self, options):
-#         #choose an action via q learning 
+    def make_a_move(self, options):
+        #choose an action via q learning 
 		
-#     def make_bet(self):
-#         #OVERWRITES PLAYERS METHOD
-#         #calcualte a bet based on current bank
+    def make_bet(self):
+        #OVERWRITES PLAYERS METHOD
+        #calcualte a bet based on current bank
 		
 class Random(Player):
     def __init__(self, name, player_num):
