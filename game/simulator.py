84eca27bf7a3755ec5ae4dc3c00800a7405de497
TommyRiffe <thomasjriffe@gmail.com>
2024-12-12 23:54:19 -0500

    this aint right

--- game/simulator.py
+++ game/simulator.py
@@ -7,7 +7,7 @@ from src.constants import *
 class Simulator:
     def __init__(self):
         ''' idk '''
-    def betting(self, bet_state: BetState):
+    def betting(self, bet_state: BetState, max_bet: int, stop_high: int, stop_low: int ):
         
         
         self.root_bet_state = bet_state
@@ -30,12 +30,23 @@ class Simulator:
             
         # explore the belief states
         for belief_state in belief_states:
-            (state, probability) = belief_states
+            (state, probability) = belief_state
             actions_bs = state.generate_action()
             for act in actions_bs:
-                
-        
+                act
 
+
+
+
+    @staticmethod            
+    def get_bet_states_from_dealer(dealer_states: list[tuple[DealerState,float]], max_bet: int, stop_high: int, stop_low: int ) -> list[BetState]:
+        dealer_states_to_process = dealer_states
+        while(len(dealer_states_to_process) > 0 ):
+            (this_state, prob) = dealer_states_to_process.pop()
+            actions = this_state.generate_actions()
+            for action in actions:
+                next_states = this_state.generate_state(action, max_bet, stop_high, stop_low)
+                for state in next_states 
     def turn(self):
         '''
         '''
