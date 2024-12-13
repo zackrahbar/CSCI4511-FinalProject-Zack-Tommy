0e18f54b80d0657c07e917318fb152adba5471e4
Zack rahbar <zackrahbar@gwu.edu>
2024-12-13 14:28:52 -0500

    is this right

--- game/simulator.py
+++ game/simulator.py
@@ -9,4 +9,18 @@ class Simulator:
 
     def betting(self):
 
+    def backprop(self, state):
+
+        starting = state
+
+        while not isinstance(starting.parent, BetState):
+            #update weigheted value of parent
+            starting.parent.weighted_value_total = starting.parent.weighted_value_total + (starting.weighted_value_total * starting.parent_tuple[2])
+
+            #update child counter of parent
+            starting.parent.childtotal = starting.parent.childtotal + 1
+
+            starting = starting.parent
+
+
     def turn(self):
